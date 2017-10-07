927d939efb7a8ca454c9e58fef6289dcc53773ea
Nik Sharma <sharma7n@gmail.com>
2017-10-07 23:35:31 +0000

    fix test

--- App.test.js
+++ App.test.js
@@ -3,7 +3,11 @@ import App from './App';
 
 import renderer from 'react-test-renderer';
 
+// Scroll was causing test to fail compilation, so mock it out.
+jest.mock('ScrollView', () => jest.genMockFromModule('ScrollView'));
+
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
+  // app is created async'ly, so use Promise.then
+  let rendered = new Promise(() => (renderer.create(<App />).toJSON()));
+  rendered.then(app => expect(app).toBeTruthy());
 });
